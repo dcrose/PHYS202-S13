f0de019720c88e9e8b67a1247892d8e3936d4372
dcrose <dcrose@calpoly.edu>
2013-04-30 13:55:45 -0700

    Adding things.

--- Week4/Electrostatics.py
+++ Week4/Electrostatics.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def pointPotential(x,y,q,posx,posy):
     """Return the point potential of a particle given the x and y values."""
-    k = 8.98755*10**9; q = 1.6e-19
+    k = 8.98755*10**9; q = 1.6*10**(-19)
     Vpoint = (k*q/((x-posx)**2+(y-posy)**2)**(1/2.))
     return Vpoint
 
-def dipolePotential(x,y,q1,q2,d):
+def dipolePotential(x,y,q,d):
     """Returns the dipole potential of a particle given the x and y values."""
     k = 8.98755*10**9;
-    Vdipole = (k*q1/(x**2+(y-(d/2))**2)**(1/2.))-(k*q2/(x**2+(y+(d/2))**2)**(1/2.))
+    Vdipole = (k*q/(y**2+(x-(d/2))**2)**(1/2.))-(k*q/(y**2+(x+(d/2))**2)**(1/2.))
     return Vdipole
